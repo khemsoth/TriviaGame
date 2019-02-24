457edc4cd05dbdaf7a8a102b7117968638760c08
Keith Hemsoth <kevhemsoth@gmail.com>
2019-02-23 20:09:44 -0500

    timer work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,27 @@
+var clock;
+var timer = 45;
 
-$(document).ready(function() {
-function startTimer() {
-    var timer = setTimeout(timer, 4000);
-    console.log("4 seconds");
+window.onload = function() {
+  $("button[name='start']").click(countdown);
 }
+  function countdown() {
+    timer--;
+    $("#timer").text("Time Remaining: 00:" + timer);
+    if(timer === 0) {
+      stopTimer();
+    }
+  }
+  function stopTimer() {
+    clearInterval(clock);
+    $("#timer").text("Time Remaining: 00:00");
+  }
 
-$("#startButton").click(function() {
-    startTimer();
-    $("#timer").innerHTML(timer);
-})})
+  function submitClick() {
+      stopTimer();
+      $("#timer").text("Time Remaining: 00:00");
+    }
+    clock = setInterval(countdown, 1000);
+
+$("button[name='submitButton']").click(function() {
+  submitClick();
+})
