f233359aef0c8d6bafefd1239c55606b318b0073
unknown <kevhemsoth@gmail.com>
2019-02-26 17:59:55 -0500

    display ques/ans function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -62,43 +62,29 @@ window.onload = function() {
     }
   }
 function displayAnswers() {
-    function displayChoices1() {
       for(var i = 0; i < 25; i++) {
-        $("#q" + i).text(questions[0].choices[i]);
+        $("#q" + i).append(questions[0].choices[i]);
       }
-    }
-    function displayChoices2() {
-      for(var j = 0; j < questions.length; j++) {
-        $("#q" + i).text(questions[1].choices[j]);
+      for(var j = 2; j < 25; j++) {
+        $("#q" + j).text(questions[1].choices[j - 2]);
       }
-    }
-    function displayChoices3() {
-      for(var k = 0; k < questions.length; k++) {
-        $("#q" + i).text(questions[2].choices[k]);
+      for(var k = 6; k < 25; k++) {
+        $("#q" + k).text(questions[2].choices[k - 6]);
       }
-    }
-    function displayChoices4() {
-      for(var l = 0; l < questions.length; l++) {
-        $("#q" + i).text(questions[3].choices[l]);
+      for(var l = 10; l < 25; l++) {
+        $("#q" + l).text(questions[3].choices[l - 10]);
       }
-    }
-    function displayChoices5() {
-      for(var m = 0; m < questions.length; m++) {
-        $("#q" + i).text(questions[4].choices[m]);
+      for(var m = 14; m < 25; m++) {
+        $("#q" + m).text(questions[4].choices[m - 14]);
       }
-    }
-    function displayChoices6() {
-      for(var o = 0; o < questions.length; o++) {
-        $("#q" + i).text(questions[5].choices[o]);
+      for(var n = 18; n < 25; n++) {
+        $("#q" + n).text(questions[5].choices[n - 18]);
       }
-    }
-    function displayChoices7() {
-      for(var p = 0; p < questions.length; p++) {
-        $("#q" + i).text(questions[6].choices[p]);
+      for(var o = 22; o < 25; o++) {
+        $("#q" + o).text(questions[6].choices[o - 22]);
       }
-    }
   }
-}
+
 
 
     clock = setInterval(countdown, 1000);
@@ -107,5 +93,4 @@ function displayAnswers() {
 
 $("button[name='submitButton']").click(function() {
   submitClick();
-})
-}
+})}
